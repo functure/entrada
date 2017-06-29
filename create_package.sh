96af4ba189088bf9e9649e2c889e3f48c01a3f85
functure <halilburaknoyan@gmail.com>
2017-06-29 17:47:22 +0300

    maven packaging added to create_package.sh

--- create_package.sh
+++ create_package.sh
@@ -4,6 +4,13 @@ VERSION=$(cat VERSION)
 BASE_DIR="entrada-$VERSION"
 echo "Create ENTRADA installation package for version $VERSION"
 
+cd dnslib4java
+mvn install
+cd ../pcaplib4java
+mvn install
+cd ../pcap-to-parquet
+mvn install
+
 mkdir $BASE_DIR
 cp pcap-to-parquet/target/pcap-to-parquet-$VERSION-jar-with-dependencies.jar $BASE_DIR
 cp -R scripts $BASE_DIR
